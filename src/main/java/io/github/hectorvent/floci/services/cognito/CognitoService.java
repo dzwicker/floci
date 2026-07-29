49f5cece510507840c082a438fe22a41808d2cd2
Daniel Zwicker <zwicker.daniel@broeskamp.com>
2026-07-29 12:36:33 +0200

    fix(cognito): align attribute verification with AWS

--- src/main/java/io/github/hectorvent/floci/services/cognito/CognitoService.java
+++ src/main/java/io/github/hectorvent/floci/services/cognito/CognitoService.java
@@ -2600,7 +2600,7 @@ public class CognitoService implements ResourceProvider {
         String jti = extractJtiFromToken(accessToken);
 
         if (username == null || poolId == null || jti == null) {
-            throw new AwsException("NotAuthorizedException", "Invalid access token", 400);
+            throw new AwsException("NotAuthorizedException", "Invalid Access Token", 400);
         }
 
         validateTokenNotRevoked(jti, poolId, "access");
@@ -2610,13 +2610,14 @@ public class CognitoService implements ResourceProvider {
 
         if (!"email".equals(attributeName) && !"phone_number".equals(attributeName)) {
             throw new AwsException("InvalidParameterException",
-                    "Only email and phone_number attributes can be verified", 400);
+                    "Invalid attribute name. Only phone_number and email can be verified.", 400);
         }
 
         CognitoUser user = adminGetUser(poolId, username);
         if (blankToNull(user.getAttributes().get(attributeName)) == null) {
             throw new AwsException("InvalidParameterException",
-                    "User has no " + attributeName + " attribute set", 400);
+                    "Unable to verify attribute: " + attributeName + " no value set to verify",
+                    400);
         }
 
         VerificationCode.Purpose purpose = "email".equals(attributeName)
